502aaa3b0b13b3160593586d79284846268745fa
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-11-14 17:32:33 +0100

    Cleaned up energycheck etc

--- HackathonBackend/Models/EnergyCheck.cs
+++ HackathonBackend/Models/EnergyCheck.cs
@@ -1,12 +1,9 @@
 ﻿namespace HackathonBackend.Models
 {
-  
-using System;
-
-public class UserInfo
+    public class EnergyCheck
     {
         public int Id { get; set; }
-        public int ResourceId { get; set; }
+        public int ResourceId { get; set; } // This will be the foreign key to UserId
         public DateTime AtDateTime { get; set; }
         public double ActiveEnergyOutlet { get; set; }
         public double ActiveEffectOutlet { get; set; }
@@ -16,19 +13,24 @@ public class UserInfo
         public double PhaseCurrent2 { get; set; }
         public double PhaseCurrent3 { get; set; }
 
-        public EnergyData(int id, int resourceId, DateTime atDateTime, double activeEnergyOutlet, double activeEffectOutlet,
-                          double activeEnergyInput, double activeEffectInput, double phaseCurrent1,
-                          double phaseCurrent2, double phaseCurrent3)
-        {
-            Id = id;
-            ResourceId = resourceId;
-            AtDateTime = atDateTime;
-            ActiveEnergyOutlet = activeEnergyOutlet;
-            ActiveEffectOutlet = activeEffectOutlet;
-            ActiveEnergyInput = activeEnergyInput;
-            ActiveEffectInput = activeEffectInput;
-            PhaseCurrent1 = phaseCurrent1;
-            PhaseCurrent2 = phaseCurrent2;
-            PhaseCurrent3 = phaseCurrent3;
-        }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
+}
+
+//public EnergyData(int id, int resourceId, DateTime atDateTime, double activeEnergyOutlet, double activeEffectOutlet,
+//                          double activeEnergyInput, double activeEffectInput, double phaseCurrent1,
+//                          double phaseCurrent2, double phaseCurrent3)
+//        {
+//            Id = id;
+//            ResourceId = resourceId;
+//            AtDateTime = atDateTime;
+//            ActiveEnergyOutlet = activeEnergyOutlet;
+//            ActiveEffectOutlet = activeEffectOutlet;
+//            ActiveEnergyInput = activeEnergyInput;
+//            ActiveEffectInput = activeEffectInput;
+//            PhaseCurrent1 = phaseCurrent1;
+//            PhaseCurrent2 = phaseCurrent2;
+//            PhaseCurrent3 = phaseCurrent3;
+//        }
+//    }
