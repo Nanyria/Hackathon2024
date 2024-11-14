65a56cb28d81c69d033aa0470990a55f38902291
Sofie Nørby Noes Møller <58098@student.eadania.dk>
2024-11-14 13:41:02 +0100

    jo

--- HackathonBackend/Models/ApplianceEnergyUsage.cs
+++ HackathonBackend/Models/ApplianceEnergyUsage.cs
@@ -6,6 +6,8 @@
         public double EnergyUsage { get; set; }
         public int ApplianceId { get; set; }
         public Appliance? Appliance { get; set; }
-        
+        // Indicate appliance energy usage for recommendation purposes.
+        public bool IsHighEnergy { get; set; } = false;
+
     }
 }
