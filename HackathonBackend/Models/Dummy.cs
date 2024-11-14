731c04c8fb317c837f8aeaba30bca5f6abbf27c7
Sofie Nørby Noes Møller <58098@student.eadania.dk>
2024-11-14 16:37:08 +0100

    jo

--- HackathonBackend/Models/Dummy.cs
+++ HackathonBackend/Models/Dummy.cs
@@ -1,22 +1,54 @@
 
-namespace HackathonBackend.Data
+namespace HackathonBackend.Models
 {
-    public static class DummyDataGenerator
+    public static class DummyData
     {
-        public static EnergyProfile GenerateDummyEnergyProfile()
+        public static List<EnergyProfile> DummyEnergyProfiles()
         {
+            // Fetch the list of appliances
             var appliances = ApplianceData.GetAppliances();
 
-            // Manually select specific ApplianceIds 
-            var selectedApplianceIds = new List<int> { 2, 4, 12, 13, 16, 19 };
-
-            return new EnergyProfile
+            // List of different EnergyProfiles, each with manually selected ApplianceIds
+            var energyProfiles = new List<EnergyProfile>
             {
-                EnergyProfileId = 1,
-                ApplianceIds = selectedApplianceIds,
-                Date = DateTime.Now
-                // Or add data point as reference
+                new EnergyProfile
+                {
+                    EnergyProfileId = 1,
+                    ApplianceIds = new List<int> { 2, 4, 12, 13, 16, 19 }, // Profile with specific appliances 
+                    Date = DateTime.Now
+                    // Or add data point as reference
+                },
+                new EnergyProfile
+                {
+                    EnergyProfileId = 2,
+                    ApplianceIds = new List<int> { 1, 3, 5, 9, 15 }, // Different set of appliances
+                    Date = DateTime.Now
+                    // Or add data point as reference
+                },
+                new EnergyProfile
+                {
+                    EnergyProfileId = 3,
+                    ApplianceIds = new List<int> { 7, 8, 10, 14, 17 },
+                    Date = DateTime.Now
+                    // Or add data point as reference
+                },
+                new EnergyProfile
+                {
+                    EnergyProfileId = 4,
+                    ApplianceIds = new List<int> { 6, 11, 18, 20, 25 },
+                    Date = DateTime.Now
+                    // Or add data point as reference
+                },
+                new EnergyProfile
+                {
+                    EnergyProfileId = 5,
+                    ApplianceIds = new List<int> { 22, 23, 24 },
+                    Date = DateTime.Now
+                    // Or add data point as reference
+                }
             };
+
+            return energyProfiles;
         }
     }
 }
