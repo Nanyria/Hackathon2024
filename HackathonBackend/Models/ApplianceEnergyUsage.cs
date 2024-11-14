bde69cc92eb67d20902b706fbf83e1b73701d058
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-11-14 10:18:56 +0100

    added classes for appliances

---
+++ HackathonBackend/Models/ApplianceEnergyUsage.cs
@@ -0,0 +1,10 @@
+﻿namespace HackathonBackend.Models
+{
+    public class ApplianceEnergyUsage
+    {
+        public int ApplianceEnergyUsageId { get; set; }
+        public double EnergyUsage { get; set; }
+        public int ApplianceId { get; set; }
+        public Appliance? Appliance { get; set; }
+    }
+}
